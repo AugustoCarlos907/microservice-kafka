faec5a0bb38e9c600b5b7f1374aa8296c4ab6859
Augusto Carlos <augustoaccarlos@gmail.com>
2026-06-22 13:19:14 +0100

    first commit

--- src/test/java/com/services/vendaservice/VendaServiceApplicationTests.java
+++ src/test/java/com/services/vendaservice/VendaServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class VendaServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
